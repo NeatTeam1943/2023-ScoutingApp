499055cb9706aadda5cf3eb6393b5881a5a3113d
Guy Avital <guyavital10@gmail.com>
2023-02-04 16:51:44 +0200

    enums: Add match related enums

---
+++ lib/enums/charge_station.dart
@@ -0,0 +1,5 @@
+enum ChargeStationState {
+  docked,
+  engaged,
+  failed,
+}
---
+++ lib/enums/driver_station.dart
@@ -0,0 +1,8 @@
+enum DriverStation {
+  first(1),
+  second(2),
+  third(3);
+
+  const DriverStation(this.value);
+  final int value;
+}
