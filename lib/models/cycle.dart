bed8a5bee1e5f2a8dd481f9713f50197113c2d37
Guy Avital <guyavital10@gmail.com>
2023-02-04 16:48:27 +0200

    models: Add cycle model

---
+++ lib/models/cycle.dart
@@ -0,0 +1,25 @@
+// Project imports:
+import 'package:neatteam_scouting_2023/enums/game_piece.dart';
+import 'package:neatteam_scouting_2023/enums/grid_level.dart';
+import 'package:neatteam_scouting_2023/enums/grid_zone.dart';
+import 'package:neatteam_scouting_2023/enums/pickup_zone.dart';
+import 'package:neatteam_scouting_2023/models/team.dart';
+
+class Cycle {
+  Cycle({
+    required this.cycleNumber,
+    this.isHalf = false,
+  })  : isDefended = false,
+        isSuccessful = true;
+
+  int cycleNumber;
+  bool isDefended;
+  Team? defendingTeam;
+  GamePiece? gamePiece;
+  PickupZone? pickupZone;
+  GridLevel? gridLevel;
+  GridZone? gridZone;
+  double? cycleTime;
+  bool isSuccessful;
+  bool isHalf;
+}
