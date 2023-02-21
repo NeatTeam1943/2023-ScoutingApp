5f96ecb309670eb71e64d54732b96990b0b95a41
Guy Avital <guyavital10@gmail.com>
2023-02-21 16:43:08 +0200

    utils, pages: Add option to share generated CSV file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,16 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:provider/provider.dart';
+
 // Project imports:
+import 'package:neatteam_scouting_2023/match_data_csv.dart';
+import 'package:neatteam_scouting_2023/models/match.dart';
+import 'package:neatteam_scouting_2023/models/team.dart';
+import 'package:neatteam_scouting_2023/providers/matches_provider.dart';
 import 'package:neatteam_scouting_2023/utils/frc_teams.dart';
-import '../models/team.dart';
+import 'package:neatteam_scouting_2023/utils/utils.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -33,22 +40,46 @@ class _HomePageState extends State<HomePage> {
         centerTitle: false,
         actions: [
           TextButton.icon(
+            onPressed: _saveCSV,
+            icon: const Icon(Icons.share),
+            label: const Text("Share"),
+            style: TextButton.styleFrom(foregroundColor: Colors.white),
+          ),
+          TextButton.icon(
             onPressed: () => Navigator.pushNamed(context, '/add-match'),
             icon: const Icon(Icons.add),
             label: const Text("Game"),
             style: TextButton.styleFrom(foregroundColor: Colors.white),
-          )
+          ),
         ],
       ),
       body: Center(
         child: ListView.builder(
           itemCount: _teams.length,
           itemBuilder: (_, index) => ListTile(
-            title: Text(_teams[index].name!),
+            title: Text(_teams[index].name ?? ''),
             subtitle: Text('#${_teams[index].number}'),
           ),
         ),
       ),
     );
+  }
+
+  void _saveCSV() {
+    List<Match> matches =
+        Provider.of<MatchesProvider>(context, listen: false).matches;
+
+    if (matches.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No matches data found'),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+      return;
+    }
+
+    String csv = makeMatchDataCSV(matches);
+    saveToDownloads(filename: 'match-data.csv', content: csv);
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,29 @@
+// Dart imports:
+import 'dart:developer';
+import 'dart:io';
+
+// Flutter imports:
 import 'package:flutter/services.dart';
+
+// Package imports:
+import 'package:path_provider/path_provider.dart';
+import 'package:share_extend/share_extend.dart';
 
 Future<String> loadAsset(String path) async {
   return await rootBundle.loadString(path);
+}
+
+void saveToDownloads({
+  required String filename,
+  required String content,
+}) async {
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    final file =
+        await File('${directory.path}/$filename').writeAsString(content);
+
+    ShareExtend.share(file.path, 'file');
+  } catch (e) {
+    log('Failed sharing CSV, reason:\n$e', level: 1000);
+  }
 }
