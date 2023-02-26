ff78a9645d12671b68024ee74e993cade07135ee
Kfir Nevo <nevo.kfir@gmail.com>
2023-02-26 16:01:10 +0200

    pages: Update home page for the new match provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,12 +7,10 @@ import 'package:provider/provider.dart';
 // Project imports:
 import 'package:neatteam_scouting_2023/match_data_csv.dart';
 import 'package:neatteam_scouting_2023/models/match.dart';
-import 'package:neatteam_scouting_2023/models/team.dart';
 import 'package:neatteam_scouting_2023/providers/matches_provider.dart';
-import 'package:neatteam_scouting_2023/utils/frc_teams.dart';
 import 'package:neatteam_scouting_2023/utils/utils.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
 
   static const routeName = '/';
@@ -20,52 +18,46 @@ class HomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<Team> _teams = [];
-
-  @override
-  void initState() {
-    super.initState();
-    FrcTeams.getAll().then((teams) => setState(() => _teams = teams));
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: false,
-        actions: [
-          TextButton.icon(
-            onPressed: _saveCSV,
-            icon: const Icon(Icons.share),
-            label: const Text("Share"),
-            style: TextButton.styleFrom(foregroundColor: Colors.white),
+    return Consumer(
+      builder: (context, model, child) {
+        List<Match> matches =
+            Provider.of<MatchesProvider>(context, listen: true).matches;
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            centerTitle: false,
+            actions: [
+              TextButton.icon(
+                onPressed: () => _saveCSV(context),
+                icon: const Icon(Icons.share),
+                label: const Text("Share"),
+                style: TextButton.styleFrom(foregroundColor: Colors.white),
+              ),
+              TextButton.icon(
+                onPressed: () => Navigator.pushNamed(context, '/add-match'),
+                icon: const Icon(Icons.add),
+                label: const Text("Game"),
+                style: TextButton.styleFrom(foregroundColor: Colors.white),
+              ),
+            ],
           ),
-          TextButton.icon(
-            onPressed: () => Navigator.pushNamed(context, '/add-match'),
-            icon: const Icon(Icons.add),
-            label: const Text("Game"),
-            style: TextButton.styleFrom(foregroundColor: Colors.white),
+          body: Center(
+            child: ListView.builder(
+              itemCount: matches.length,
+              itemBuilder: (_, index) => ListTile(
+                title: Text('Match #${matches[index].number}'),
+                subtitle: Text(
+                    '${matches[index].team.name} #${matches[index].team.number}'),
+              ),
+            ),
           ),
-        ],
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: _teams.length,
-          itemBuilder: (_, index) => ListTile(
-            title: Text(_teams[index].name ?? ''),
-            subtitle: Text('#${_teams[index].number}'),
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 
-  void _saveCSV() {
+  void _saveCSV(context) {
     List<Match> matches =
         Provider.of<MatchesProvider>(context, listen: false).matches;
 
