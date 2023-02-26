76728317b560ba6855bf54effbd2325bab50e7d0
Kfir Nevo <nevo.kfir@gmail.com>
2023-02-26 16:01:22 +0200

    pages: Update match page for the new match provider

--- lib/pages/match_page.dart
+++ lib/pages/match_page.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Project imports:
-import 'package:neatteam_scouting_2023/models/match.dart';
+import 'package:neatteam_scouting_2023/utils/match_state.dart';
 
 class MatchPage extends StatefulWidget {
   const MatchPage({super.key});
@@ -13,12 +13,11 @@ class MatchPage extends StatefulWidget {
   State<StatefulWidget> createState() => _MatchPageState();
 }
 
-class _MatchPageState extends State<MatchPage> {
+class _MatchPageState extends MatchState<MatchPage> {
   @override
   Widget build(BuildContext context) {
-    final gameObj = ModalRoute.of(context)!.settings.arguments as Match;
     return Scaffold(
-      appBar: AppBar(title: Text('Match #${gameObj.number}')),
+      appBar: AppBar(title: Text('Match #${match.number}')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {},
