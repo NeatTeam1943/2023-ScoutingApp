9f0046c7cfe142b0eaf55743cf7a6fa73d2e1c11
Guy Avital <guyavital10@gmail.com>
2023-03-18 01:16:19 +0200

    widgets: Add sliding segmented control

--- lib/widgets/charge_state_field.dart
+++ lib/widgets/charge_state_field.dart
@@ -1,53 +1,27 @@
 // Flutter imports:
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:neatteam_scouting_2023/enums/charge_station.dart';
+import 'package:neatteam_scouting_2023/widgets/sliding_segmented_control.dart';
 
 class ChargeStateField extends StatelessWidget {
-  ChargeStateField({super.key, required this.onChange, this.value});
+  const ChargeStateField({super.key, required this.onChange, this.value});
 
   final Function(ChargeStationState?) onChange;
   final ChargeStationState? value;
 
-  final _changeStateColors = <ChargeStationState, Color>{
-    ChargeStationState.failed: Colors.redAccent,
-    ChargeStationState.engaged: Colors.blueAccent,
-    ChargeStationState.docked: Colors.greenAccent
-  };
-
-  ChargeStationState get _value => value ?? ChargeStationState.failed;
-
   @override
   Widget build(BuildContext context) {
-    return CupertinoSlidingSegmentedControl(
-      children: const <ChargeStationState, Widget>{
-        ChargeStationState.failed: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            'Failed',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        ChargeStationState.engaged: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            'Engaged',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        ChargeStationState.docked: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            'Docked',
-            style: TextStyle(color: Colors.white),
-          ),
-        )
+    return SlidingSegmentedControl<ChargeStationState>(
+      value: value,
+      onChange: onChange,
+      segments: ChargeStationState.values,
+      colors: const {
+        ChargeStationState.failed: Colors.redAccent,
+        ChargeStationState.engaged: Colors.blueAccent,
+        ChargeStationState.docked: Colors.greenAccent
       },
-      groupValue: value,
-      thumbColor: _changeStateColors[_value]!,
-      onValueChanged: onChange,
     );
   }
 }
