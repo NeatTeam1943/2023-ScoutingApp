a9fcf83ee2456637c4fa9e2eb15047ea05c61e3b
Guy Avital <guyavital10@gmail.com>
2023-03-18 17:20:28 +0200

    pages: endgame: Add stopwatch

--- lib/pages/endgame_page.dart
+++ lib/pages/endgame_page.dart
@@ -1,6 +1,9 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+
 // Project imports:
 import 'package:neatteam_scouting_2023/enums/charge_station.dart';
 import 'package:neatteam_scouting_2023/models/endgame.dart';
@@ -21,12 +24,24 @@ class EndgamePage extends StatefulWidget {
 class EndgameState extends MatchState<EndgamePage> {
   final _formKey = GlobalKey<FormState>();
 
+  int currentTime = 0;
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer(
+    mode: StopWatchMode.countUp,
+  );
+
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       if (snapshot.endgame == null) {
         updateMatch((m) => m.endgame = Endgame());
+      }
+
+      if (snapshot.endgame!.dockedTime == null) {
+        _stopWatchTimer.onStartTimer();
+        _stopWatchTimer.rawTime.listen((time) => currentTime = time);
+      } else {
+        currentTime = (snapshot.endgame!.dockedTime! * 1000).toInt();
       }
     });
   }
@@ -35,50 +50,66 @@ class EndgameState extends MatchState<EndgamePage> {
       match.endgame != null ? match.endgame!.didChargeStation : false;
 
   void _setChargeStationState(ChargeStationState css) {
-    updateMatch((m) => m.endgame!.chargeStationState = css);
+    updateMatch((m) {
+      m.endgame!.chargeStationState = css;
+      m.endgame!.dockedTime = currentTime / 1000.0;
+    });
+  }
+
+  void onPop() {
+    if (snapshot.endgame!.chargeStationState == null) {
+      snapshot.endgame!.chargeStationState = ChargeStationState.failed;
+      snapshot.endgame!.dockedTime = currentTime / 1000.0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const InGameActionBar(
-        title: Text('Endgame'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            StyleFormField(
-              field: CheckboxListTile(
-                value: didChargeStation,
-                onChanged: (value) {
-                  updateMatch((m) {
-                    m.endgame?.didChargeStation = value!;
-                  });
-                },
-                title: const Text("Charged"),
+    return WillPopScope(
+      onWillPop: () async {
+        onPop();
+        return true;
+      },
+      child: Scaffold(
+        appBar: InGameActionBar(
+          onFinish: onPop,
+          match: match,
+          title: StreamBuilder<int>(
+            stream: _stopWatchTimer.rawTime,
+            builder: (context, snap) {
+              final displayTime =
+                  StopWatchTimer.getDisplayTime(currentTime, hours: false);
+
+              return Text("Endgame\n$displayTime");
+            },
+          ),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              // Did Attempt Stabilizing field
+              StyleFormField(
+                field: CheckboxListTile(
+                  value: didChargeStation,
+                  onChanged: (value) {
+                    updateMatch((m) {
+                      m.endgame?.didChargeStation = value!;
+                    });
+                  },
+                  title: const Text("Did attempt stabilizing?"),
+                ),
               ),
-            ),
-            ChargeStateField(
-              value: match.endgame?.chargeStationState,
-              onChange: (value) {
-                _setChargeStationState(value!);
-              },
-            ),
-            StyleFormField(
-              field: TextFormField(
-                autofocus: false,
-                initialValue: (snapshot.endgame?.dockedTime ?? 0.0).toString(),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  updateMatch(
-                      (m) => m.endgame?.dockedTime = double.tryParse(value));
+
+              // Charge Station State field
+              ChargeStateField(
+                value: match.endgame?.chargeStationState,
+                onChange: (value) {
+                  _setChargeStationState(value!);
                 },
-                decoration: const InputDecoration(
-                    labelText: "Time docked (in seconds)"),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
